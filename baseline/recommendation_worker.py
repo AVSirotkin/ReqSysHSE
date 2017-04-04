47628a2b155adcc32206d133ac7ad60de42da395
Stanislav Kozlov <sekozlov@edu.hse.ru>
2017-04-04 15:01:56 +0300

    Рабочий бейзлайн

--- baseline/recommendation_worker.py
+++ baseline/recommendation_worker.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python3
 '''
 Build recommendations based on trained XGBoost model
 
 by Daniel Kohlsdorf
 '''
 
-from model import *
-import xgboost as xgb
 import numpy as np
+import xgboost as xgb
+
+from model import *
 
 TH = 0.8
+
 
 def classify_worker(item_ids, target_users, items, users, output_file, model):
     with open(output_file, 'w') as fp:
@@ -17,17 +18,24 @@ def classify_worker(item_ids, target_users, items, users, output_file, model):
         average_score = 0.0
         num_evaluated = 0.0
         for i in item_ids:
-            data = []        
-            ids  = []
-
+            data = []
+            ids = []
+            if i not in items:
+                continue
             # build all (user, item) pair features based for this item
-            for u in target_users:        
+            for u in target_users:
+                # print(u)
+                # print(users[u])
+                # print(items[i])
+                if u not in users:
+                    continue
                 x = Interaction(users[u], items[i], -1)
                 if x.title_match() > 0:
                     f = x.features()
                     data += [f]
-                    ids  += [u]
+                    ids += [u]
 
+            # print(data)
             if len(data) > 0:
                 # predictions from XGBoost
                 dtest = xgb.DMatrix(np.array(data))
@@ -37,22 +45,24 @@ def classify_worker(item_ids, target_users, items, users, output_file, model):
                 average_score += sum(ypred)
                 num_evaluated += float(len(ypred))
 
-                # use all items with a score above the given threshold and sort the result
+                # use all items with a score above the given threshold and sort
+                # the result
                 user_ids = sorted(
                     [
                         (ids_j, ypred_j) for ypred_j, ids_j in zip(ypred, ids) if ypred_j > TH
                     ],
-                    key = lambda x: -x[1]
-                )[0:99]                                                        
+                    key=lambda x: -x[1]
+                )[0:99]
 
                 # write the results to file
-                if len(user_ids) > 0:            
+                if len(user_ids) > 0:
                     item_id = str(i) + "\t"
                     fp.write(item_id)
                     for j in range(0, len(user_ids)):
                         user_id = str(user_ids[j][0]) + ","
                         fp.write(user_id)
                     user_id = str(user_ids[-1][0]) + "\n"
+                    fp.write(user_id)
                     fp.flush()
 
             # Every 100 iterations print some stats
@@ -62,5 +72,5 @@ def classify_worker(item_ids, target_users, items, users, output_file, model):
                 except ZeroDivisionError:
                     score = 0
                 percentageDown = str(pos / float(len(item_ids)))
-                print(output_file + " " + percentageDown + " " + score)
-            pos += 1        
+                print(output_file + " " + percentageDown + " " + str(score))
+            pos += 1
--- baseline/xgb.py
+++ baseline/xgb.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 '''
 Baseline solution for the ACM Recsys Challenge 2017
 using XGBoost
@@ -6,47 +5,50 @@ using XGBoost
 by Daniel Kohlsdorf
 '''
 
-import xgboost as xgb
-import numpy as np
-import multiprocessing
 
+
+import numpy as np
+import xgboost as xgb
+'''
+from model import Interaction, Item, User
+from parser import InteractionBuilder, select
+'''
 from model import *
 from parser import *
 from recommendation_worker import *
-import random
+# import random
 
 print(" --- Recsys Challenge 2017 Baseline --- ")
 
-N_WORKERS         = 5
+
 USERS_FILE        = "/students/iamishalkin/shared/ReqSys/data/users.csv"
 ITEMS_FILE        = "/students/iamishalkin/shared/ReqSys/data/items.csv"
 INTERACTIONS_FILE = "/students/iamishalkin/shared/ReqSys/data/interactions.csv"
-TARGET_USERS      = "/students/iamishalkin/shared/ReqSys/data/targetUsers.csv"
-#TARGET_USERS='targetUsers_W_H.csv'
-TARGET_ITEMS      = "/students/iamishalkin/shared/ReqSys/data/targetItems.csv"
 
 
 '''
 1) Parse the challenge data, exclude all impressions
    Exclude all impressions
 '''
-(header_users, users) = select(USERS_FILE, lambda x: True, build_user, lambda x: int(x[0]))
-(header_items, items) = select(ITEMS_FILE, lambda x: True, build_item, lambda x: int(x[0]))
+(header_users, users) = select(USERS_FILE,
+                               lambda x: True, build_user, lambda x: int(x[0]))
+(header_items, items) = select(ITEMS_FILE,
+                               lambda x: True, build_item, lambda x: int(x[0]))
 
 builder = InteractionBuilder(users, items)
 (header_interactions, interactions) = select(
     INTERACTIONS_FILE,
-    lambda x: x[2] != '0',  
+    lambda x: x[2] != '0',
     builder.build_interaction,
-    lambda x: (int(x[0]), int(x[1])) 
+    lambda x: (int(x[0]), int(x[1]))
 )
 
 
 '''
 2) Build recsys training data
 '''
-data    = np.array([interactions[key].features() for key in interactions.keys()])
-labels  = np.array([interactions[key].label() for key in interactions.keys()])
+data = np.array([interactions[key].features() for key in interactions.keys()])
+labels = np.array([interactions[key].label() for key in interactions.keys()])
 dataset = xgb.DMatrix(data, label=labels)
 dataset.save_binary("recsys2017.buffer")
 
@@ -55,73 +57,18 @@ dataset.save_binary("recsys2017.buffer")
 3) Train XGBoost regression model with maximum tree depth of 2 and 25 trees
 '''
 evallist = [(dataset, 'train')]
-param = {'bst:max_depth': 2, 'bst:eta': 0.1, 'silent': 1, 'objective': 'reg:linear' }
-param['nthread']     = 4
-param['eval_metric'] = 'rmse'
-param['base_score']  = 0.0
-num_round            = 25
+param = {'bst:max_depth': 2,
+         'bst:eta': 0.1,
+         'silent': 1,
+         'objective': 'reg:linear',
+         }
+
+param.update({
+    'nthread': 4,
+    'eval_metric': 'rmse',
+    'base_score': 0.0
+})
+print(param)
+num_round = 25
 bst = xgb.train(param, dataset, num_round, evallist)
 bst.save_model('recsys2017.model')
-
-
-'''
-4) Create target sets for items and users
-'''
-target_users = []
-
-
-with open(TARGET_USERS) as target_users_without_header:
-    next(target_users_without_header) #skip the header
-    for line in target_users_without_header:
-        target_users += [int(line.strip())]
-
-
-
-#for line in open(TARGET_USERS):
-#    target_users += [int(line.strip())] 
-    
-target_users = set(target_users)
-
-target_items = []
-for line in open(TARGET_ITEMS):
-    target_items += [int(line.strip())]
-    
-
-
-'''
-5) Schedule classification
-'''
-
-
-
-
-filename = "solution.csv"
-classify_worker(target_items, target_users, items, users, filename, bst)
-
-'''
-bucket_size = len(target_items) / N_WORKERS
-start = 0
-jobs = []
-
-for i in range(0, N_WORKERS):
-  stop = int(min(len(target_items), start + bucket_size))
-  filename = "solution_" + str(i) + ".csv"
-  classify_worker(target_items[start:stop], target_users, items, users, filename, bst)
-  start=stop
-'''
-
-
-"""
-for i in range(0, N_WORKERS):
-    stop = int(min(len(target_items), start + bucket_size))
-    filename = "solution_" + str(i) + ".csv"
-    process = multiprocessing.Process(target = classify_worker, args=(target_items[start:stop], target_users, items, users, filename, bst))
-    jobs.append(process)
-    start = stop
-
-for j in jobs:
-    j.start()
-
-for j in jobs:
-    j.join()
-"""
